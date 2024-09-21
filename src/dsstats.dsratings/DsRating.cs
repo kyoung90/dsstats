4be228424ad2ee16c2e6ca9b2f9fd84821683b92
ipax77 <ph.ilipp@web.de>
2024-09-21 08:30:57 +0200

    DsRatingCalculator

---
+++ src/dsstats.dsratings/DsRating.cs
@@ -0,0 +1,20 @@
+﻿namespace dsstats.dsratings;
+
+public class DsRating
+{
+    public int Pos { get; set; }
+    public double PercentileRank { get; set; }
+    public int Games { get; set; }
+    public int Wins { get; set; }
+    public int Duration { get; set; }
+    public double Rating {  get; set; }
+    public double Consistency { get; set; }
+    public double Confidence { get; set; }
+    public double RecentRatingGain { get; set; }
+    public double PeakRating {  get; set; }
+    public int WinStreak { get; set; }
+    public int LoseStreak { get; set; }
+    public int CurrentStreak { get; set; }
+}
+
+
---
+++ src/dsstats.dsratings/DsRatingCalculator.cs
@@ -0,0 +1,85 @@
+﻿using dsstats.shared;
+using dsstats.shared.Calc;
+using System.Collections.Frozen;
+
+namespace dsstats.dsratings;
+
+public abstract class DsRatingCalculator
+{
+    public async Task CalculateRatings()
+    {
+        var replaysRequest = new CalcReplaysRequest() { Skip = 0, Take = 5000 };
+        var ratingRequest = new CalcRatingRequest()
+        {
+            RatingCalcType = RatingCalcType.Dsstats,
+            MmrIdRatings = new()
+                    {
+                        { 1, new() },
+                        { 2, new() },
+                        { 3, new() },
+                    { 4, new() }
+                    },
+            BannedPlayers = new Dictionary<PlayerId, bool>().ToFrozenDictionary()
+        };
+
+        var replays = await GetReplays(replaysRequest);
+
+        while (replays.Count > 0)
+        {
+            List<ReplayDsRatingResult> replayRatings = [];
+            foreach (var replay in replays)
+            {
+                var replayRatingDto = ProcessReplay(replay, ratingRequest);
+                if (replayRatingDto is not null && !replay.IsArcade)
+                {
+                    replayRatings.Add(replayRatingDto);
+                }
+            }
+
+            await SaveStepResult(replayRatings, ratingRequest);
+
+            replaysRequest.Skip += replaysRequest.Take;
+            replays = await GetReplays(replaysRequest);
+        }
+
+        await SavePlayerRatings(ratingRequest);
+    }
+
+
+    public abstract Task<List<CalcDto>> GetReplays(CalcReplaysRequest request);
+    public abstract ReplayDsRatingResult ProcessReplay(CalcDto replay, CalcRatingRequest request);
+    public abstract Task SavePlayerRatings(CalcRatingRequest request);
+    public abstract Task SaveStepResult(List<ReplayDsRatingResult> replayRatings, CalcRatingRequest request);
+
+}
+
+public record CalcReplaysRequest
+{
+    public int Skip { get; set; }
+    public int Take { get; set; }
+}
+
+public record ReplayDsRatingResult
+{
+    public int RatingType { get; set; }
+    public int LeaverType { get; init; }
+    public double ExpectationToWin { get; init; } // WinnerTeam
+    public int ReplayId { get; set; }
+    public bool IsPreRating { get; set; }
+    public List<ReplayPlayerDsRatingResult> PlayerRatings { get; init; } = [];
+}
+
+public class ReplayPlayerDsRatingResult
+{
+    public int Games { get; set; }
+    public int Wins { get; set; }
+    public int Duration { get; set; }
+    public double Rating { get; set; }
+    public double Consistency { get; set; }
+    public double Confidence { get; set; }
+    public double RecentRatingGain { get; set; }
+    public double PeakRating { get; set; }
+    public int WinStreak { get; set; }
+    public int LoseStreak { get; set; }
+    public int CurrentStreak { get; set; }
+}
